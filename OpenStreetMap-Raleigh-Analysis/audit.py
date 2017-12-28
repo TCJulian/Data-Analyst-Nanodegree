84b51ca0dc02b87e24459540f4f3cb420f04b3b4
Tyler C. Julian <tylercjulian@live.com>
2017-12-28 17:26:15 -0500

    Update audit.py
    * Added internal functions to reduce copy/pasting.
    * Moved some code around to have to correct affect on the element.
    * Add functionality for creating a text file lists the changes made by the "audit_postcode" function.

--- OpenStreetMap-Raleigh-Analysis/audit.py
+++ OpenStreetMap-Raleigh-Analysis/audit.py
@@ -2,33 +2,43 @@ import json
 import re
 
 def audit_postcodes(element, tag):
+    # Internal function that searches for postcode tags and performs an audit.
+    def check_postcode(tag_type):
+        for tags in element[tag_type]:
+            if tags['type'] == 'addr' and tags['key'] == 'postcode':
+                old_value = tags['value']
+                if postal_re_zip4.search(old_value):
+                    new_value = old_value[:5]
+                    tags['value'] = new_value
+                    bad_zip4.append([old_value, new_value])
+                elif postal_re_char.search(old_value):
+                    new_value = old_value
+                    new_value = re.sub('[a-z]', '', new_value,  flags=re.IGNORECASE)
+                    tags['value'] = new_value
+                    bad_char.append([old_value, new_value])
+                elif len(old_value) != 5:
+                    new_value = old_value[:5]
+                    tags['value'] = new_value
+                    unknown.append([old_value, new_value])
+        return element
+
+    # re formats used in the function above.
     postal_re_zip4 = re.compile('[-]')
     postal_re_char = re.compile('[^0-9]')
 
+    # Lists to store any changes made to the original data.
     bad_zip4 = []
     bad_char = []
     unknown = []
-    auditted_codes = {"Zip+4":bad_zip4, "Characters":bad_char, "Unknown":unknown}
 
     if tag == "node":
-        if element['node_tags']['type'] == 'addr' and element['node_tags']['key'] == 'postcode':
-            old_value = element['node_tags']['value']
-            if postal_re_zip4.search(old_value):
-                new_value = old_value[:5]
-                element['node_tags']['value'] = new_value
-                bad_zip4.append([old_value, new_value])
-            elif postal_re_char.search(old_value):
-                new_value = old_value
-                re.sub('[a-z]', '', re.IGNORECASE, new_value)
-                element['node_tags']['value'] = new_value
-                bad_char.append([old_value, new_value])
-            elif len(old_value) != 5:
-                unknown.append([old_value, "Error. Can't handle this exception."])
-        
+        element = check_postcode("node_tags")
     elif tag == "way":
-        pass
-    
-    with open("changelist.txt", "w) as file:
-        file.write(json.dumps(auditted_codes))
-        
+        element = check_postcode("way_tags")
+
+    # Writes the changes to a text file and returns audited element.
+    if len(bad_zip4) or len(bad_char) or len(unknown) > 0:
+        with open("changelist.txt", "a") as file:
+            auditted_codes = {"Zip+4":bad_zip4, "Characters":bad_char, "Unknown":unknown}
+            file.write(json.dumps(auditted_codes) + "\n")
     return element
