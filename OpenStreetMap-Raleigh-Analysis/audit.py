18d76204e2f7fd27a20a15014de9f9f7f3a0d175
Tyler C. Julian <tylercjulian@live.com>
2017-12-29 13:33:03 -0500

    Added audit_streetnames function
    Function passes initial parsing, but does operate as intended:
    
    * Doesn't pass updated values.
    * Changelist writes values that have not changed.
    * "null" values in changelist

--- OpenStreetMap-Raleigh-Analysis/audit.py
+++ OpenStreetMap-Raleigh-Analysis/audit.py
@@ -1,3 +1,4 @@
+import collections
 import json
 import re
 
@@ -42,4 +43,67 @@ def audit_postcodes(element, tag):
         for key, value in auditted_codes.items():
             if len(value) > 0:
                 file.write(key + ":" + json.dumps(value) + "\n")
+    return element
+
+
+def audit_streetnames(element, tag):
+    def audit(tag_type):
+        for tags in element[tag_type]:
+            if tags['type'] == 'addr' and tags['key'] == 'street':
+                old_value = tags['value']
+                new_value = audit_street_type(old_value)
+                changelist.append([old_value, new_value])
+        return element
+
+    def audit_street_type(street_name):
+        m = street_type_re.search(street_name)
+        if m:
+            street_type = m.group()
+            if street_type not in expected:
+                new_name = update_name(street_name, mapping)
+                return new_name
+        else:
+            return street_name
+
+
+    def update_name(name, mapping):
+        for map in mapping:
+            if map in name:
+                name = name.replace(map, mapping[map])
+                return name
+        reply = "***Update Mapping***"
+        return reply
+
+    street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+    expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square",
+                "Lane", "Road", "Trail", "Parkway", "Commons", "Circle", "Way", 
+                "Parkway", "Highway", "Loop", "Run", "Crossing"]
+
+    mapping = { "St": "Street",
+                "St.": "Street",
+                "Ave": "Avenue",
+                "Rd": "Road",
+                "Rd.": "Road",
+                "Ct": "Court",
+                "Ct.": "Court",
+                "Ln": "Lane",
+                "Ln.": "Lane",
+                "Blvd": "Boulevard",
+                "Blvd.": "Boulevard",
+                "Dr": "Drive",
+                "Dr.": "Drive"
+                }
+
+    changelist = []
+
+    if tag == "node":
+        element = audit("node_tags")
+    elif tag == "way":
+        element = audit("way_tags")
+
+    # Writes the changes to a text file and returns audited element.
+    with open("streetname_changelist.txt", "a") as file:
+        if len(changelist) > 0 and changelist[0][0] != changelist[0][1]:
+            file.write(json.dumps(changelist) + "\n")
     return element
