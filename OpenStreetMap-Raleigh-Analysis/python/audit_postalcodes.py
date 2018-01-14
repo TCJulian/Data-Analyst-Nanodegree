0430fa1d258312be1036ef791c9f1e665e4c4f6d
Tyler C. Julian <tylercjulian@live.com>
2018-01-14 13:13:39 -0500

    Consolidated lists, trimmed json module, doc strings

--- OpenStreetMap-Raleigh-Analysis/python/audit_postalcodes.py
+++ OpenStreetMap-Raleigh-Analysis/python/audit_postalcodes.py
@@ -1,36 +1,36 @@
-import collections
-import json
 import re
+from json import dumps
 
-def audit_postcodes(element, tag):
-    # Internal function that searches for postcode tags and performs an audit.
+def audit_postalcodes(element, tag):
+    """Audits postal codes, writes changelist to file, and returns an updated element.
+
+    Keyword arguments:
+    element -- A shaped element, created by `osm_to_cvs` script
+    tag -- The tag type (`node` or `way`) as a string
+    """
+    
     def audit(tag_type):
+        """Checks each `postcode` tag value, updates value in shaped element, and adds changes to changelist."""
         for tags in element[tag_type]:
             if tags['type'] == 'addr' and tags['key'] == 'postcode':
                 old_value = tags['value']
                 if postal_re_zip4.search(old_value):
                     new_value = old_value[:5]
                     tags['value'] = new_value
-                    bad_zip4.append([old_value, new_value])
+                    changelist.append([old_value, new_value])
                 elif postal_re_char.search(old_value):
-                    new_value = old_value
-                    new_value = re.sub('[a-z]', '', new_value,  flags=re.IGNORECASE)
+                    new_value = re.sub('[a-z]', '', old_value,  flags=re.IGNORECASE)
                     tags['value'] = new_value
-                    bad_char.append([old_value, new_value])
+                    changelist.append([old_value, new_value])
                 elif len(old_value) != 5:
-                    new_value = old_value[:5]
-                    tags['value'] = new_value
-                    unknown.append([old_value, new_value])
+                    new_value = "***Unknown format. No changes made.***"
+                    changelist.append([old_value, new_value])
         return element
 
-    # re formats check for dashes and any characters besides numbers.
     postal_re_zip4 = re.compile('[-]')
     postal_re_char = re.compile('[^0-9]')
 
-    # Lists to store any changes made to the original data.
-    bad_zip4 = []
-    bad_char = []
-    unknown = []
+    changelist = []
 
     if tag == "node":
         element = audit("node_tags")
@@ -38,9 +38,7 @@ def audit_postcodes(element, tag):
         element = audit("way_tags")
 
     # Writes the changes to a text file and returns audited element.
-    with open("postcode_changelist.txt", "a") as file:
-        auditted_codes = {"Zip+4":bad_zip4, "Characters":bad_char, "Unknown":unknown}
-        for key, value in auditted_codes.items():
-            if len(value) > 0:
-                file.write(key + ":" + json.dumps(value) + "\n")
+    with open("changelist_postcode.txt", "a") as file:
+        if len(changelist) > 0:
+            file.write(dumps(changelist) + "\n")
     return element
