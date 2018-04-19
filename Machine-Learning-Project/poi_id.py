e9283486f6401e30fbb2396388517eb70998f557
Tyler Julian <tcjulian1717@live.com>
2018-04-19 18:58:36 -0400

    Update ML

--- Machine-Learning-Project/poi_id.py
+++ Machine-Learning-Project/poi_id.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python
 
 import sys
+import numpy as np
 import pickle
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from tester import test_classifier
 
-### Task 1: Select what features you'll use.
+#########################
+### Feature Selection ###
+#########################
+
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages',  'from_poi_to_this_person', 'from_messages',
-'from_this_person_to_poi', 'shared_receipt_with_poi'] # You will need to use more features
+
+### Below is a MASTER LIST used for testing:
+
+#features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'from_poi_to_this_person', 'from_messages',
+#'from_this_person_to_poi', 'shared_receipt_with_poi', 'from_poi_to_this_person_ratio', 'from_this_person_to_poi_ratio']
+
+### Final features for classifier
+features_list = ['poi', 'loan_advances', 'bonus', 'exercised_stock_options',  'from_this_person_to_poi_ratio']
+
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -19,12 +31,18 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 print("Load dict")
 
-### Task 2: Remove outliers
+#######################
+### Remove outliers ###
+#######################
+
 data_dict.pop('TOTAL')
 
 print("Remove outliers")
 
-### Task 3: Create new feature(s)
+###########################
+### Create new features ###
+###########################
+
 # Create 'from_this_person_to_poi_ratio':
 for key in data_dict.keys():
     from_p_to_poi = data_dict[key]['from_this_person_to_poi']
@@ -55,15 +73,14 @@ print("Create new features")
 
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
-#print(my_dataset['METTS MARK'])
-#print(len(my_dataset))
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
 print("Extracted features and labels")
 
-### Plotting code for feature exploration
+### Plotting code used for feature exploration
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -72,24 +89,22 @@ f2 = []
 for ii in features:
     f1.append(ii[0])
     f2.append(ii[1])
-f1 = np.array(f1)
-f2 = np.array(f2)
 
 fig, ax = plt.subplots()
 plt.scatter(f1, f2, s=2, c=labels)
-plt.show()
+#plt.show()
 
 print("Plotted features")
 
 ### Scale data
-
-
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 features = scaler.fit_transform(features)
 
+###################
+### Classifiers ###
+###################
 
-### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
@@ -97,46 +112,63 @@ features = scaler.fit_transform(features)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(min_samples_split=2)
-clf1 = GaussianNB()
-clf1 = LinearSVC()
+from sklearn.model_selection import GridSearchCV
+
+### Dictionaries of parameters used for algorithms in GridSearchCV
+param_tree = {'min_samples_split':[2, 3, 4, 5, 6, 7, 8, 9, 10], 'max_depth':[5, 10, 15, 20, 50]}
+param_svm = {'C': [1, 10, 100, 1000], 'kernel': ['linear', 'rbf']}
+
+### Different classifiers used for testing
+#clf = GridSearchCV(DecisionTreeClassifier(), param_tree)
+#clf = GridSearchCV(SVC(), param_svm)
+#clf = DecisionTreeClassifier(min_samples_split=2,  max_depth=5)
+#clf = GaussianNB()
+clf = SVC(kernel='rbf', C=1)
 
 print("Created classifiers")
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall
+##############################
+### Classifier Performance ###
+##############################
+
+### Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
 ### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Recursive feature slection for LinearSVM experimentation
+### Recursive feature slection for LinearSVM experimentation
 from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import RFECV
 #refcv = RFECV(estimator=clf, cv=StratifiedKFold(2), scoring='accuracy')
 #refcv.fit(features, labels)
 #print(refcv.n_features_)
 
-# Univariate feature selection for Naive Bayes
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-selector = SelectKBest(chi2, k='all').fit(features, labels)
-features = selector.transform(features)
-scores = selector.scores_
-
-print("Performed feature selection")
-
+### train_test_split method
 from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.2, random_state=42)
+    train_test_split(features, labels, test_size=0.1, random_state=42)
 
+# Univariate feature selection for Naive Bayes and Decision Tree
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+#selector = SelectKBest(chi2, k='all').fit(features_train, labels_train)
+#features_train = selector.transform(features_train)
+#f_scores = np.int64(selector.scores_)
+#print(f_scores)
+#print("Performed feature selection")
 
+### Fit classfier and test results
+#clf.fit(features_train, labels_train)
+#features_test = selector.transform(features_test)
+#score = clf.score(features_test, labels_test)
+#print(clf.best_params_)
+#print(score)
 
-clf.fit(features_train, labels_train)
-score = clf.score(features_test, labels_test)
-print(score)
+test_classifier(clf, my_dataset, features_list)
 
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
--- Machine-Learning-Project/resources.md
+++ Machine-Learning-Project/resources.md
@@ -2,3 +2,5 @@
 
 * [Accuracy, Precision, and Recall](http://blog.exsilio.com/all/accuracy-precision-recall-f1-score-interpretation-of-performance-measures/)
 * [Feature Selection](https://machinelearningmastery.com/an-introduction-to-feature-selection/)
+* Stack Overflow
+    * [GridSearchCV for decision trees](https://stackoverflow.com/questions/31161637/grid-search-cross-validation-in-sklearn/31162095#31162095)
